a45a368b7ebde83b7e4d8303d1bcad96682b5162
javabeanz <java.development@gmail.com>
2016-06-29 02:21:10 +0200

    clean up imports and warnings

--- deepviolet-api/src/main/java/com/mps/deepviolet/suite/Protocol.java
+++ deepviolet-api/src/main/java/com/mps/deepviolet/suite/Protocol.java
@@ -1,49 +1,41 @@
 package com.mps.deepviolet.suite;
 
 import java.util.HashMap;
-import java.util.Map.Entry;
-import java.util.Set;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Protocol {
-	
-	private static final Logger logger = LoggerFactory.getLogger("com.mps.deepviolet.suite.Protocol");
-	
+
 	private String name;
-	
-	private HashMap<String,Object> mciphers = new HashMap<String,Object>();
-	
-	public Protocol( String name ) {
-		
+
+	private HashMap<String, Object> mciphers = new HashMap<String, Object>();
+
+	public Protocol(String name) {
+
 		this.name = name;
-		
+
 	}
-	
+
 	public String getName() {
-		
+
 		return name;
-		
+
 	}
-	
-	public void addCipher( String cipher ) {
-		
+
+	public void addCipher(String cipher) {
+
 		mciphers.put(cipher, null);
-		
+
 	}
-	
+
 	public String[] getCiphers() {
-		
-		return (String[])mciphers.keySet().toArray(new String[0]);
-		
-		
+
+		return (String[]) mciphers.keySet().toArray(new String[0]);
+
 	}
-	
-	public boolean hasCipher( String cipher ) {
-		
+
+	public boolean hasCipher(String cipher) {
+
 		return mciphers.containsKey(cipher);
-		
+
 	}
-	 
+
 }
