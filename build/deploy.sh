f94c20be8e7760304b3447ae3cc4291c5ce477ca
milton.smith <milton.smith@oracle.com>
2016-11-23 20:26:41 -0800

    Fix bug in .travis.yml 4

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,5 +2,5 @@
 #
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-mvn deploy -Dgpg.defaultKeyring=false -Dgpg-keyname=56D2CBF1 -Dgpg.passphrase=${KEYRING_PASSPHRASE} -Dgpg.publicKeyring=~/build/pubring.gpg -Dgpg.secretKeyring=~/build/secring.gpg --settings ~/settings.xml
+mvn deploy -Dgpg.defaultKeyring=false -Dgpg-keyname=56D2CBF1 -Dgpg.passphrase=${KEYRING_PASSPHRASE} -Dgpg.publicKeyring=build/pubring.gpg -Dgpg.secretKeyring=build/secring.gpg --settings ~/settings.xml
 fi
