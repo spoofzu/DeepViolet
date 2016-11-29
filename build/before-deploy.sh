36370f426142b61f7d19b9b1b2f26e202a9cc703
milton.smith <milton.smith@oracle.com>
2016-11-29 12:03:32 -0800

    Trying to get Maven CD working

--- build/before-deploy.sh
+++ build/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	openssl aes-256-cbc -K $GPG_KEY_NAME -iv $GPG_PASSPHRASE -in build/signingkey.asc.enc -out build/signingkey.asc -d
+	echo $OPENSSL_ENCRYPT_KEY | openssl aes-256-cbc -a -salt -in build/signingkey.asc.enc -out build/signingkey.asc -d 
 	gpg --fast-import build/signingkey.asc
 fi
