0a623fed813cb68ed14fe554bbf37d4787b7aada
milton.smith <milton.smith@oracle.com>
2016-11-29 17:56:24 -0800

    More Mavin CD testing

--- build/before-deploy.sh
+++ build/before-deploy.sh
@@ -2,5 +2,6 @@
 #
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	openssl aes-256-cbc -a -in build/signingkey.asc.enc -out build/signingkey.asc -d -k $OPENSSL_ENCRYPT_KEY
-	gpg --fast-import build/signingkey.asc
+	#gpg --fast-import build/signingkey.asc
+	gpg -v --batch --import build/signingkey.asc
 fi
