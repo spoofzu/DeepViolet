fdeeacaa2017ac13914bef5b06c4cabf3c200ec5
milton.smith <milton.smith@oracle.com>
2016-11-29 22:24:07 -0800

    Travis Mavin CD testing

--- build/before-deploy.sh
+++ build/before-deploy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 #
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	openssl aes-256-cbc -a -in build/signingkey.asc.enc -out build/signingkey.asc -d -k $OPENSSL_ENCRYPT_KEY
-	gpg -v --batch --import build/signingkey.asc
+	openssl aes-256-cbc -a -in build/pubring.gpg.enc -out build/pubring.gpg -d -k $OPENSSL_ENCRYPT_KEY
+	openssl aes-256-cbc -a -in build/secring.gpg.enc -out build/secring.gpg -d -k $OPENSSL_ENCRYPT_KEY
+	rm ~/.gnupg/pubring.gpg
+	rm ~/.gnupg/secring.gpg
+	mv build/pubring.gpg ~/.gnupg/pubring.gpg
+	mv build/secring.gpg ~/.gnupg/secring.gpg
+	#gpg -v --batch --import build/signingkey.asc
 	gpg --list-keys
+	echo $GPG_PASSPHRASE | gpg -ab --passphrase-fd 0 pom.xml 
 fi
