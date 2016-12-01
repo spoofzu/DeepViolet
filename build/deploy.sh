7499bbb8de3da0dc7b04a825fa8c5869954f2ab3
milton.smith <milton.smith@oracle.com>
2016-12-01 12:22:18 -0800

    Travis Maven CI/CD testing22

--- build/deploy.sh
+++ build/deploy.sh
@@ -5,6 +5,8 @@ set -e -u
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	echo "Publishing Maven snapshot..."
-	mvn clean deploy -P sign,build-extras --settings="settings.xml" -Dmaven.test.skip=true 
+	mvn clean deploy -P sign,build-extras --settings="settings.xml" -Dmaven.test.skip=true \
+	  -Dgpg.passphrase="$GPG_PASSPHRASE" -Dgpg.keyname="$GPG_KEYNAME" -Dgpg.executable="gpg2" \
+      -Dgpg.homedir="~/.gnupg"
     echo "Maven snapshot published..."
 fi
