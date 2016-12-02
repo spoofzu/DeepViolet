1d3d6868dcbfe9e378258f956091d8495455a893
milton.smith <milton.smith@oracle.com>
2016-12-01 19:45:50 -0800

    Maven Travis CI/CD testing33

--- build/before-deploy.sh
+++ build/before-deploy.sh
@@ -3,13 +3,14 @@
 
 set -e -u
 
-echo "HOME folder is $HOME"
+echo "User HOME folder is $HOME"
 gpg2 --version
-gpg2 --list-keys 
-gpg2 --list-secret-keys
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-		#gpg -v --batch --import build/signingkey.asc
+
 	openssl aes-256-cbc -pass pass:$OPENSSL_ENCRYPT_KEY -in build/private-key.gpg.enc -out build/private-key.gpg -d
 	gpg2 -v --batch --import build/private-key.gpg
 fi
+
+gpg2 --list-keys 
+gpg2 --list-secret-keys
