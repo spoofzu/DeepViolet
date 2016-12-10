7c67989f6b3c69182eea5d8be6e4503bf54f5fca
milton.smith <milton.smith@oracle.com>
2016-12-09 17:19:02 -0800

    Travis and Maven testing continued3

--- build/before-deploy.sh
+++ build/before-deploy.sh
@@ -8,7 +8,7 @@ gpg --version
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 
-	openssl aes-256-cbc -a -in build/private-key.gpg.enc -out build/private-key.gpg -d -pass pass:$OPENSSL_ENCRYPT_KEY
+	openssl aes-256-cbc -a -in build/private-key.gpg.enc -out build/private-key.gpg -d -k $OPENSSL_ENCRYPT_KEY
 	gpg -v --batch --import build/private-key.gpg
 	
 fi
