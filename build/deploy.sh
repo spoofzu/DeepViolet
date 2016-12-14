e753fa0a6c50c0d7c8ed10b685d82f171eb50f42
milton.smith <milton.smith@oracle.com>
2016-12-13 18:39:31 -0800

    Maven Travis CI/CD testing119

--- build/deploy.sh
+++ build/deploy.sh
@@ -6,7 +6,8 @@ set -e
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	echo "***Publishing Maven snapshot..."
 	mvn --batch-mode -X deploy -P sign,build-extras --settings="./settings.xml" -Dmaven.test.skip=true \
-		 -Darguments=-Dgpg.passphrase="I love Mac." -Dgpg.passphrase="I love Mac."
+		 -Darguments=-Dgpg.passphrase="{1ytJn1Uv10gHLO/URDhNnZZgm0pIYpGSbk8h9mOPE+w=}" \
+	     -Dgpg.passphrase="{1ytJn1Uv10gHLO/URDhNnZZgm0pIYpGSbk8h9mOPE+w=}"
     echo "***Maven snapshot published..."
 fi
 
