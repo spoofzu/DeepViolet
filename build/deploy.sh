429d8b1107f89f4a9550bd008122cfcc7607606f
milton.smith <milton.smith@oracle.com>
2016-12-15 22:27:28 -0800

    Maven Travis CI/CD testing150

--- build/deploy.sh
+++ build/deploy.sh
@@ -10,11 +10,11 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
 	echo "*** deploy.sh, deploying release."
 	
 	#note: milton 12/2/2016, this is not optimial but keyrings are also password encrypted
-	mvn --batch-mode -X release:prepare release:perform -P sign,build-extras --settings="./settings.xml" \
+	mvn --batch-mode -X clean release:prepare release:perform -P sign,build-extras --settings="./settings.xml" \
 		 -Dmaven.test.skip=true \
 	     -Darguments=-Dgpg.passphrase="I love Mac." \
          -Dgpg.passphrase="I love Mac."
-	     #-DconnectionUrl="scm:git:git@github.com:spoofzu/DeepViolet.git"
+	     -DconnectionUrl="scm:git:https://${GH_TOKEN}@github.com/spoofzu/DeepViolet.git"
 	
 	#note: milton 12/2/2016, this is not optimial but keyrings are also password encrypted
 	#mvn --batch-mode -X deploy -P sign,build-extras --settings="./settings.xml" -Dmaven.test.skip=true \
