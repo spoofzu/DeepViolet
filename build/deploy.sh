bdc836c28aafd98c6933a72a2e56e6bb29df7ade
milton.smith <milton.smith@oracle.com>
2016-12-19 13:59:06 -0800

    Maven Travis CI/CD testing183

--- build/deploy.sh
+++ build/deploy.sh
@@ -8,23 +8,21 @@ set -e
 # note: milton 12/17/2016, Very important, reserved bash shell characters must be escaped
 #                          with a slash.
 
-# Exit on any github tag that is a release.
-if  [[ "$TRAVIS_TAG" =~ "^v.*$"  ]]; then
-	echo "*** deploy.sh, release detected, skipping deploy/release."
-	exit 0;
-fi
-
-# Don't release unless merging pull request to "master".
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+#
+# Do if: new tag
+#
+if [ ! -z "$TRAVIS_TAG" ]; then
 	
 	echo "*** deploy.sh, deploying release."
 	
-	mvn release:prepare release:perform --settings="settings.xml" \
+	# 
+	# Set version in pom.xml to $TRAVIS_TAG
+	#
+	mvn --settings="settings.xml" org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion="$TRAVIS_TAG" -Prelease \
 		 -Dmaven.test.skip=true \
 	     -Darguments=-Dgpg.passphrase="I\ love\ Mac." \
 	     -B
-		 #-DdryRun=true \
-	     #-DconnectionUrl="scm:git:https://${GH_TOKEN}@github.com/spoofzu/DeepViolet.git"
+		 #-DdryRun=true
 
 	echo "*** deploy.sh, deployment complete."
 fi
